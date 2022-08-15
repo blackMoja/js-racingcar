292dc283556e85a84029aef477e6568d653cadb6
blackmoja <limlim980625@gmail.com>
2022-08-16 01:43:29 +0900

    feat: early return 적용

--- src/core/move.js
+++ src/core/move.js
@@ -4,31 +4,35 @@ import getRandomNumber from '../utils/getRandomNumber.js'
 
 const move = async () => {
   let gameCount = state.gameCount
-
-  const interval = setInterval(async () => {
-    if (gameCount > 0) {
-      gameCount = gameCount - 1
-      await sleep(1000)
-      runProcess(gameCount)
-    } else {
-      clearInterval(interval)
-    }
-  }, 1000)
+  // TODO while과 동일한 프로세스를 적용해볼 수 있도록 수정하기 @blackmoja
+  // const interval = setInterval(async () => {
+  //   if (gameCount > 0) {
+  //     gameCount = gameCount - 1
+  //     await sleep(1000)
+  //     runProcess(gameCount)
+  //   } else {
+  //     clearInterval(interval)
+  //   }
+  // }, 1000)
+  while (gameCount > 0) {
+    gameCount = gameCount - 1
+    await sleep(1000)
+    runProcess(gameCount)
+  }
 }
 
 let result = []
 const runProcess = (gameCount) => {
   state.cars.forEach((car, index) => {
     const getMove = getRandomNumber()
-    const { name, position } = car
+    let { name, position: _position } = car
 
-    let _position = position
-
-    if (getMove >= 4) {
-      result[index] = { name, position: (_position = _position + 1) }
-    } else {
-      result[index] = { name, position: (_position = _position) }
+    if (getMove < 4) {
+      result[index] = { name, position: _position }
+      return
     }
+
+    result[index] = { name, position: (_position = _position + 1) }
   })
 
   setState({
